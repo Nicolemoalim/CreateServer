d6e64634d694919fc46e4323719e47931cc0a259
Nicolemoalim <nicolemoalim9@gmail.com>
2025-01-08 14:40:14 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const dirPath = path.join(__dirname, "/templates");
+const file2Send = fs.readFileSync(`${dirPath}/page.html`, "utf-8"); // תוקן השימוש ב-Template Literals
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.end(file2Send);
+});
+
+server.listen(3001, () => {
+  console.log("Server working on port 3001");
+});
